9fb6e9241a65f357c06e5fa64f3642b32097fb59
Sheikh Sohan <princesohan751@gmail.com>
2024-02-06 12:46:04 +0600

    fixed graphics styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03b05c399c2d59fe"; }
+/******/ 		__webpack_require__.h = function() { return "8996a010c624a77e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
