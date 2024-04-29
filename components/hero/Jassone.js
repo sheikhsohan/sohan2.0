edeae76bbbf20663882ad4e0316595e7b6b6a766
Sheikh Sohan <104677321+sheikhsohan@users.noreply.github.com>
2024-04-29 10:20:03 +0600

    Update Jassone.js

--- components/hero/Jassone.js
+++ components/hero/Jassone.js
@@ -24,7 +24,7 @@ const Jassone = ({ activeMenu }) => {
               A passionate Web Designer and Web Developer based In Khulna, Bangladesh
             </p>
             <div className="iknow_tm_button yellow transition_link">
-              <a href="#contact">Work with me</a>
+              <a href="#contact">Download My App</a>
             </div>
           </div>
           <div className="right">
